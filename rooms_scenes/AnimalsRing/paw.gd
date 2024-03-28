00246b43f74646f343bdb91b84aeced7ee4ec86b
ellenohara0922 <150261208+ellenohara0922@users.noreply.github.com>
2024-03-28 21:17:33 +0000

    Delete rooms_scenes/AnimalsRing directory

--- rooms_scenes/AnimalsRing/paw.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody3D
-
-
-signal label_clicked()
-func pointer_event(event:XRToolsPointerEvent):
-	if event.event_type==XRToolsPointerEvent.Type.RELEASED:
-		emit_signal("label_clicked")
